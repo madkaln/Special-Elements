b8eb6b6f9d55edf5d2662e4398059d9a215b5797
mad kalna <madara.k@hotmail.co.uk>
2018-08-02 08:25:52 +0100

    1. Created Input Text box -methots to help find and input text into the text box. Also example of how to return max length

---
+++ InteractionWithWebsite/TextInputField.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+class TextInputField
+{
+    /*This part of code represents how to access text box
+     * initialising drivers outside main for the code to access it globally
+     */
+
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement textBox;
+
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/special-elements/text-input-field/";
+        driver.Navigate().GoToUrl(url);
+
+        textBox = driver.FindElement(By.Name("username"));
+        textBox.SendKeys("Test Text");
+
+        Thread.Sleep(3000);
+
+        //Have to specify value as otherwise return an empty string
+        //Console.WriteLine(textBox.GetAttribute("value"));
+
+        //Return max length available in the text box
+
+        Console.WriteLine(textBox.GetAttribute("maxlength"));
+
+        Thread.Sleep(3000);
+
+        driver.Quit();
+
+    }
+}
+
